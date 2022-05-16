dd126318cfc4b3ea482b62350e63938c90e0b1e2
dfilastro <d.filastro@hotmail.com>
2022-05-16 20:34:58 -0300

    c2

---
+++ ReactJS/Chapter II/I - Class/src/components/Summary/index.tsx
@@ -0,0 +1,72 @@
+import { useTransactions } from '../../hooks/useTransactions';
+
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
+import { Container } from './styles';
+
+export function Summary() {
+  const { transactions } = useTransactions();
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.outcome += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
+
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt='Entradas' />
+        </header>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.income)}
+        </strong>
+      </div>
+      <div>
+        <header>
+          <p>Saidas</p>
+          <img src={outcomeImg} alt='Entradas' />
+        </header>
+        <strong>
+          -
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.outcome)}
+        </strong>
+      </div>
+      <div className='highlight-background'>
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt='Total' />
+        </header>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.total)}
+        </strong>
+      </div>
+    </Container>
+  );
+}
