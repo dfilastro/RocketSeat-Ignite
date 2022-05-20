4a2033f3087bd077402b20a33e912cbedd57c58d
dfilastro <d.filastro@hotmail.com>
2022-05-20 11:15:08 -0300

    removed some unused importation and commented code

--- ReactJS/Chapter III/II - Challenge/src/services/prismic.ts
+++ ReactJS/Chapter III/II - Challenge/src/services/prismic.ts
@@ -2,14 +2,9 @@ import * as prismic from '@prismicio/client';
 import { HttpRequestLike } from '@prismicio/client';
 import { enableAutoPreviews } from '@prismicio/next';
 
-import { NextApiRequest } from 'next';
-
 export interface PrismicConfig {
   req?: HttpRequestLike;
 }
-
-// export const endpoint = process.env.PRISMIC_API_EDNPOINT;
-// export const repositoryName = prismic.getRepositoryName(endpoint);
 
 export function getPrismicClient(config: PrismicConfig): prismic.Client {
   const client = prismic.createClient(process.env.PRISMIC_API_EDNPOINT);
